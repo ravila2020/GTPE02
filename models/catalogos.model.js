1cf6930da5024dd3dde2c66e669441a27e385b3d
ravila2020 <59663271+ravila2020@users.noreply.github.com>
2022-03-08 16:22:40 -0600

    Create catalogos.model.js

---
+++ models/catalogos.model.js
@@ -0,0 +1,7 @@
+const DictamenesModel = function(datDictamenes) {
+    this.IdDictamen = datDictamenes.IdDictamen;
+    this.Nombre = datDictamenes.Nombre;
+    this.Estatus = datDictamenes.Estatus;
+};
+
+module.exports = { DictamenesModel };
